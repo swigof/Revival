0ea0b4e3b472281c2683a52f65683add43dfb943
SwigOf <33262954+SwigOf@users.noreply.github.com>
2025-10-14 12:33:06 -0400

    add view jitter, remove controls border

--- src/page.gd
+++ src/page.gd
@@ -3,6 +3,10 @@ extends AnimatedSprite2D
 var page_above: AnimatedSprite2D
 var page_below: AnimatedSprite2D
 var count: int = 0
+var rng = RandomNumberGenerator.new()
+const page_home = Vector2(363, 452)
+const jitter_rate = 1.0 / 24
+var jitter_timer = 0
 
 func _create_sound_controls() -> Node2D:
 	var sound_controls_scene = load("res://src/sound_controls.tscn")
@@ -12,14 +16,21 @@ func _create_sound_controls() -> Node2D:
 	return sound_controls
 
 func _ready() -> void:
-	page_above = get_node("PageAbove")
-	page_below = get_node("PageBelow")
+	page_above = $PageAbove
+	page_below = $PageBelow
 	var first_page = Sprite2D.new()
 	first_page.texture = load("res://assets/title.png")
 	first_page.scale.x = 0.5
 	first_page.scale.y = 0.5
 	page_above.add_child(first_page)
 	page_above.add_child(_create_sound_controls())
+
+func _process(delta: float) -> void:
+	jitter_timer += delta
+	if jitter_timer >= jitter_rate:
+		var jitter = Vector2(randf_range(-0.25, 0.25), rng.randf_range(-0.25, 0.25))
+		position = page_home + jitter
+		jitter_timer = 0
 
 func _on_next_page_button_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton && event.button_mask & MOUSE_BUTTON_LEFT:

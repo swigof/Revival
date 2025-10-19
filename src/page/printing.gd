581fc9d4e0c8622a45be0285ccb8df6e78873e4a
SwigOf <33262954+SwigOf@users.noreply.github.com>
2025-10-19 17:59:05 -0400

    add printing page paper collection

--- src/page/printing.gd
+++ src/page/printing.gd
@@ -1,9 +1,26 @@
 class_name PrintingPage extends Page
 
+var paper_scene = load("res://src/page/paper.tscn")
+var paper_count = 10
+
 func _ready() -> void:
 	var label = Label.new()
 	label.text = "printing"
 	add_child(label)
 
+func _on_paper_input_event(_v: Node, event: InputEvent, _s: int, source: CollisionObject2D) -> void:
+	if event is InputEventMouseButton && event.button_mask & MOUSE_BUTTON_LEFT:
+		source.queue_free()
+		paper_count -= 1
+		if paper_count <= 0:
+			pass # TODO show next page arrow
+
 func start():
-	pass
+	var i = 0
+	while i < paper_count:
+		var paper: RigidBody2D = paper_scene.instantiate()
+		paper.apply_impulse(Vector2(randf_range(-2000, 2000), randf_range(-2000, 2000)))
+		paper.apply_torque_impulse(randf_range(-5, 5))
+		paper.get_node("Clickbox").input_event.connect(_on_paper_input_event.bind(paper))
+		add_child(paper)
+		i += 1

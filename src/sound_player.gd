b30e9266df94b490ec210f3cdb190c3e5d3bb6fd
SwigOf <33262954+SwigOf@users.noreply.github.com>
2025-10-21 10:41:36 -0400

    fix exported audio resources

--- src/sound_player.gd
+++ src/sound_player.gd
@@ -9,13 +9,9 @@ func _ready() -> void:
 	paper_throw_player.set_bus(&"SFX")
 	paper_throw_player.set_stream(paper_throw_sounds)
 	var path = "res://assets/paper-throws-flapping"
-	var dir = DirAccess.open(path)
-	dir.list_dir_begin()
-	while true:
-		var file_name = dir.get_next()
-		if file_name == "":
-			break
-		elif !file_name.ends_with(".import"):
+	var file_names = ResourceLoader.list_directory(path)
+	for file_name in file_names:
+		if file_name.ends_with(".mp3"):
 			paper_throw_sounds.add_stream(-1, load(path + "/" + file_name))
 
 func play_paper_throw_sound() -> void:

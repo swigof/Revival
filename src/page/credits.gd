daf96f39a8943e20fccc29bb76e26b1cb9eb6b45
SwigOf <33262954+SwigOf@users.noreply.github.com>
2025-10-23 10:28:29 -0400

    add further credit details, adjust formatting

--- src/page/credits.gd
+++ src/page/credits.gd
@@ -3,7 +3,8 @@ class_name Credits extends Page
 func _ready() -> void:
 	var label = Label.new()
 	label.text = "
-		Based on the film The Animal Kingdom (1932)
+		Based on the The Animal Kingdom (1932)
+		Materials used from the film are in the public domain
 
 		Niconne Regular font by Vernon Adams
 		EB Garamond font by Georg Duffner & Octavio Pardo
@@ -14,18 +15,22 @@ func _ready() -> void:
 		Arrow by Felbrigg on game-icons.net
 		Both licensed under CC BY 3.0
 
-		film grime overlay by jamal studio on Vecteezy
-		
-		Featuring the music \"Lonely\" by Leo Reisman & His Orchestra
+		Film Grime Overlay by jamal studio on Vecteezy
+		License under the Vecteezy Free License
 
-		Dialogue Manager Godot plugin by Nathan Hoad licensed under the MIT License
+		Featuring the music \"Lonely\" by Leo Reisman & His Orchestra
+		From a 1927 recording in the public domain
+
+		Dialogue Manager Godot plugin by Nathan Hoad 
+		Licensed under the MIT License
 
 		Everything else by swigof
-		
+
 		Made with Godot, Audacity, GIMP and DaVinci Resolve
 	"
-	label.position.x = -340
-	label.position.y = -360
+	label.position.x = -360
+	label.position.y = -440
+	label.custom_minimum_size = Vector2(720, 720)
 	label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 	add_child(label)
 	pass
